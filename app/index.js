60250bba0bceb4cc416302593b975b2687b17b55
Victornrib <victornrib@gmail.com>
2024-04-24 03:03:43 +0200

    Configuring proj structure

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <Text>Test</Text>
+    )
+}
+
+export default Home;
