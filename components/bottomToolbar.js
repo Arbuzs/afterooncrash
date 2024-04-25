079aa8ac7d260a42750f274fc2fdd236b0c7cab5
Tom <tb2r@icloud.com>
2024-04-25 12:54:38 +0200

    pathing bugs v2

--- components/bottomToolbar.js
+++ components/bottomToolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Image, Text, Pressable } from 'react-native';
 
-import { icons } from '../../constants';
+import { icons } from '../constants';
 import styles from '../styles/components/bottomToolbar';
 
 
